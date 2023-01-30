aa611dbfe4a40dff95feeae989f61e0640380e51
Pixelshot <pixelshot8@gmail.com>
2023-01-30 22:16:37 +0800

    meetings db in data folder. eCalendar title is up

--- app/components/Calendar.jsx
+++ app/components/Calendar.jsx
@@ -17,49 +17,7 @@ import {
 } from 'date-fns';
 import { useState } from 'react';
 import Meeting from '~/components/Meeting';
-
-const meetings = [
-  {
-    id: 1,
-    name: 'Leslie Alexander',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    startDatetime: '2022-05-11T13:00',
-    endDatetime: '2022-05-11T14:30',
-  },
-  {
-    id: 2,
-    name: 'Michael Foster',
-    imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    startDatetime: '2022-05-20T09:00',
-    endDatetime: '2022-05-20T11:30',
-  },
-  {
-    id: 3,
-    name: 'Dries Vincent',
-    imageUrl:
-      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    startDatetime: '2022-05-20T17:00',
-    endDatetime: '2022-05-20T18:30',
-  },
-  {
-    id: 4,
-    name: 'Leslie Alexander',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    startDatetime: '2022-06-09T13:00',
-    endDatetime: '2022-06-09T14:30',
-  },
-  {
-    id: 5,
-    name: 'Michael Foster',
-    imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    startDatetime: '2022-05-13T14:00',
-    endDatetime: '2022-05-13T14:30',
-  },
-];
+import { meetings } from '~/data/meetings';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
@@ -96,13 +54,16 @@ export default function Calendar() {
         <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200">
           <div className="md:pr-14">
             <div className="">
-              <div className="grid grid-rows-2 justify-items-end">
-                <h2 className="text-4xl">
-                  {format(firstDayCurrentMonth, 'yyy')}
-                </h2>
-                <h2>Time Placeholder</h2>
+              <div className="grid grid-cols-2">
+                <h2 className="text-4xl">eCalendar</h2>
+                <div className="grid grid-rows-2 justify-items-end">
+                  <h2 className="text-4xl">
+                    {format(firstDayCurrentMonth, 'yyy')}
+                  </h2>
+                  <h2>Time Placeholder</h2>
+                </div>
               </div>
-              <div className="flex items-center">
+              <div className="flex">
                 <h2 className="flex-auto font-semibold text-gray-400">
                   {format(firstDayPreviousMonth, 'MMMM')}
                 </h2>
@@ -217,6 +178,16 @@ export default function Calendar() {
   );
 }
 
+let colStartClasses = [
+  '',
+  'col-start-2',
+  'col-start-3',
+  'col-start-4',
+  'col-start-5',
+  'col-start-6',
+  'col-start-7',
+];
+
 // function Meeting({ meeting }) {
 //   let startDateTime = parseISO(meeting.startDatetime);
 //   let endDateTime = parseISO(meeting.endDatetime);
@@ -295,13 +266,3 @@ export default function Calendar() {
 //     </li>
 //   );
 // }
-
-let colStartClasses = [
-  '',
-  'col-start-2',
-  'col-start-3',
-  'col-start-4',
-  'col-start-5',
-  'col-start-6',
-  'col-start-7',
-];
