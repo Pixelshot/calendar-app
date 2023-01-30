3f0ab2007c839d18db04c1cefe1119338581980a
Pixelshot <pixelshot8@gmail.com>
2023-01-30 15:57:21 +0800

    Still working on UI part. Calendar and schedule are now at the center of the screen

--- app/components/Calendar.jsx
+++ app/components/Calendar.jsx
@@ -90,51 +90,54 @@ export default function Calendar() {
   );
 
   return (
-    <div className="pt-16">
-      <div className="max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6">
+    <div className="md:grid h-screen w-screen place-items-center">
+      <div className="px-4 mx-auto sm:px-7 md:max-w-6xl md:px-6">
         <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200">
           <div className="md:pr-14">
-            <div className="flex items-center justify-center">
-              {/* Need to change the name to reflect the year */}
-              <h2>{format(firstDayCurrentMonth, 'yyy')}</h2>
+            <div className="">
+              <div className="flex items-center justify-center">
+                <div className="grid place-content-end">
+                  <h2>{format(firstDayCurrentMonth, 'yyy')}</h2>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <h2 className="flex-auto font-semibold text-gray-400">
+                  {format(firstDayPreviousMonth, 'MMMM')}
+                </h2>
+                <h2 className="flex-auto font-semibold text-gray-900">
+                  {format(firstDayCurrentMonth, 'MMMM')}
+                </h2>
+                <h2 className="flex-auto font-semibold text-gray-400">
+                  {format(firstDayNextMonth, 'MMMM')}
+                </h2>
+                <button
+                  type="button"
+                  onClick={previousMonth}
+                  className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+                >
+                  <span className="sr-only">Previous month</span>
+                  <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
+                </button>
+                <button
+                  onClick={nextMonth}
+                  type="button"
+                  className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+                >
+                  <span className="sr-only">Next month</span>
+                  <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
+                </button>
+              </div>
             </div>
-            <div className="flex items-center">
-              <h2 className="flex-auto font-semibold text-gray-900">
-                {format(firstDayPreviousMonth, 'MMMM')}
-              </h2>
-              <h2 className="flex-auto font-semibold text-gray-900">
-                {format(firstDayCurrentMonth, 'MMMM')}
-              </h2>
-              <h2 className="flex-auto font-semibold text-gray-900">
-                {format(firstDayNextMonth, 'MMMM')}
-              </h2>
-              <button
-                type="button"
-                onClick={previousMonth}
-                className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
-              >
-                <span className="sr-only">Previous month</span>
-                <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
-              </button>
-              <button
-                onClick={nextMonth}
-                type="button"
-                className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
-              >
-                <span className="sr-only">Next month</span>
-                <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
-              </button>
+            <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500 md:w-96">
+              <div>SUN</div>
+              <div>MON</div>
+              <div>TUE</div>
+              <div>WED</div>
+              <div>THU</div>
+              <div>FRI</div>
+              <div>SAT</div>
             </div>
-            <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500">
-              <div>S</div>
-              <div>M</div>
-              <div>T</div>
-              <div>W</div>
-              <div>T</div>
-              <div>F</div>
-              <div>S</div>
-            </div>
-            <div className="grid grid-cols-7 mt-2 text-sm">
+            <div className="grid grid-cols-7 mt-2 text-sm md:md-96">
               {days.map((day, dayIdx) => (
                 <div
                   key={day.toString()}
