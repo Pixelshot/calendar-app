bee2216e1b8139b4ff171a0c8f14568127f22aa6
Pixelshot <pixelshot8@gmail.com>
2023-01-31 09:39:42 +0800

    export function classes. Removed Meeting function inside of Calendar. configure jsconfig.json for the damn error

--- app/components/Calendar.jsx
+++ app/components/Calendar.jsx
@@ -19,7 +19,7 @@ import { useState } from 'react';
 import Meeting from '~/components/Meeting';
 import { meetings } from '~/data/meetings';
 
-function classNames(...classes) {
+export function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
 
@@ -91,6 +91,8 @@ export default function Calendar() {
                 </button>
               </div>
             </div>
+            <div className="flex"></div>
+            {/* Start of Day and dates */}
             <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500 md:w-96">
               <div>SUN</div>
               <div>MON</div>
@@ -164,7 +166,7 @@ export default function Calendar() {
                   <Meeting
                     meeting={meeting}
                     key={meeting.id}
-                    classNames={classNames()}
+                    classNames={classNames}
                   />
                 ))
               ) : (
@@ -187,82 +189,3 @@ let colStartClasses = [
   'col-start-6',
   'col-start-7',
 ];
-
-// function Meeting({ meeting }) {
-//   let startDateTime = parseISO(meeting.startDatetime);
-//   let endDateTime = parseISO(meeting.endDatetime);
-
-//   return (
-//     <li className="flex items-center px-4 py-2 space-x-4 group rounded-xl focus-within:bg-gray-100 hover:bg-gray-100">
-//       <img
-//         src={meeting.imageUrl}
-//         alt=""
-//         className="flex-none w-10 h-10 rounded-full"
-//       />
-//       <div className="flex-auto">
-//         <p className="text-gray-900">{meeting.name}</p>
-//         <p className="mt-0.5">
-//           <time dateTime={meeting.startDatetime}>
-//             {format(startDateTime, 'h:mm a')}
-//           </time>{' '}
-//           -{' '}
-//           <time dateTime={meeting.endDatetime}>
-//             {format(endDateTime, 'h:mm a')}
-//           </time>
-//         </p>
-//       </div>
-//       <Menu
-//         as="div"
-//         className="relative opacity-0 focus-within:opacity-100 group-hover:opacity-100"
-//       >
-//         <div>
-//           <Menu.Button className="-m-2 flex items-center rounded-full p-1.5 text-gray-500 hover:text-gray-600">
-//             <span className="sr-only">Open options</span>
-//             <DotsVerticalIcon className="w-6 h-6" aria-hidden="true" />
-//           </Menu.Button>
-//         </div>
-
-//         <Transition
-//           as={Fragment}
-//           enter="transition ease-out duration-100"
-//           enterFrom="transform opacity-0 scale-95"
-//           enterTo="transform opacity-100 scale-100"
-//           leave="transition ease-in duration-75"
-//           leaveFrom="transform opacity-100 scale-100"
-//           leaveTo="transform opacity-0 scale-95"
-//         >
-//           <Menu.Items className="absolute right-0 z-10 mt-2 origin-top-right bg-white rounded-md shadow-lg w-36 ring-1 ring-black ring-opacity-5 focus:outline-none">
-//             <div className="py-1">
-//               <Menu.Item>
-//                 {({ active }) => (
-//                   <a
-//                     href="#"
-//                     className={classNames(
-//                       active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-//                       'block px-4 py-2 text-sm'
-//                     )}
-//                   >
-//                     Edit
-//                   </a>
-//                 )}
-//               </Menu.Item>
-//               <Menu.Item>
-//                 {({ active }) => (
-//                   <a
-//                     href="#"
-//                     className={classNames(
-//                       active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-//                       'block px-4 py-2 text-sm'
-//                     )}
-//                   >
-//                     Cancel
-//                   </a>
-//                 )}
-//               </Menu.Item>
-//             </div>
-//           </Menu.Items>
-//         </Transition>
-//       </Menu>
-//     </li>
-//   );
-// }
