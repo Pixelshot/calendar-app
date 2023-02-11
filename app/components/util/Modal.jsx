e0d745814d88fd8444b2bb5293b56b0ea7a23a35
pixelshot <pixelshot8@gmail.com>
2023-02-11 08:11:48 +0800

    Modal is working. CSS w-max was the culprit

--- app/components/util/Modal.jsx
+++ app/components/util/Modal.jsx
@@ -1,8 +1,11 @@
 export default function Modal({ children, onClose }) {
   return (
-    <div className="fixed top-0 left-0 w-max h-screen" onClick={onClose}>
+    <div
+      className="fixed top-0 left-0 w-full h-screen backdrop-blur-sm"
+      onClick={onClose}
+    >
       <dialog
-        className="fixed top-[25vh] w-[30rem] border-none bg-slate-500 rounded-xl"
+        className="fixed top-[25vh] w-[30rem] border-none bg-slate-200 rounded-xl"
         open
         onClick={(event) => event.stopPropagation()}
       >
