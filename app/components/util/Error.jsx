7d5825da287e81d6a37bb4714dc6a177752a5552
Pixelshot <pixelshot8@gmail.com>
2023-02-18 09:03:11 +0800

    Finish up Error pages. Updated TODO list

--- app/components/util/Error.jsx
+++ app/components/util/Error.jsx
@@ -1,7 +1,7 @@
 function Error({ title, children }) {
   return (
-    <div className="error">
-      <div className="icon"></div>
+    <div className="mt-14 mx-[30%]">
+      <div className="icon">Icon goes here</div>
       <h2>{title}</h2>
       {children}
     </div>
--- app/routes/appointments.jsx
+++ app/routes/appointments.jsx
@@ -14,13 +14,3 @@ export default function Index() {
 export async function loader() {
   return await getAppointments();
 }
-
-export function ErrorBoundary({ error }) {
-  console.error(error);
-  return (
-    <div>
-      <h2>Oh snap!</h2>
-      <p>There was a problem loading this appointment</p>
-    </div>
-  );
-}
