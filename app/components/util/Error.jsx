1296ae3b976f1e313d26d83998cc25437218aa41
Pixelshot <pixelshot8@gmail.com>
2023-02-20 13:08:09 +0800

    Error margin

--- app/components/util/Error.jsx
+++ app/components/util/Error.jsx
@@ -1,7 +1,7 @@
 import { TbError404Off } from 'react-icons/tb';
 function Error({ title, children }) {
   return (
-    <div className="mt-14 mx-[30%] p-10">
+    <div className="mt-14 mx-[10%] p-10">
       <TbError404Off size={200} className="mx-auto" />
       <h2>{title}</h2>
       {children}
